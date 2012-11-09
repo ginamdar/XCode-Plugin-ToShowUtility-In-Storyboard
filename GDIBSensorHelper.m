225534ec39d437dd74225a7c591ec1d02ea8b7f1
Guruprasad Inamdar <guru.inamdar@gmail.com>
2012-11-08 23:16:46 -0500

    Working Revision - 1

--- GDIBSensorHelper.m
+++ GDIBSensorHelper.m
@@ -2,8 +2,8 @@
 //  GDIBSensorHelper.m
 //  GDIBSensor
 //
-//  Created by Guru Inamdar on 2012-11-03.
-//  Copyright (c) 2012 Guruprasad Inamdar. All rights reserved.
+//  Created by Guru Inamdar on 2012-11-06.
+//  Copyright (c) 2012 Guruprasad Inamdar.
 //
 
 #import "GDIBSensorHelper.h"
@@ -20,43 +20,24 @@
 - (id)gdib_initWithNibName:(id)arg1 bundle:(id)arg2 document:(id)arg3;
 - (id)gdib_initWithNibName:(id)arg1 bundle:(id)arg2;
 
-- (void)gdib_noteUserDidExplicitlyChooseChoice:(id)arg1;
-- (void)gdib_userSelectedCategoryChoiceFromMenu:(id)arg1;
 - (id)gdib_selectedCategory;
-
-- (void)gdib_toggleUtilitiesVisibility:(id)sender;
-- (void)showUtilities;
-- (void)gdib_showUtilitiesArea:(id)sender;
 
 @end
 
 @implementation NSObject(GDIBSensorHook)
 
 #pragma mark - StoryboardPane 
+
 - (id)gdib_initWithNibName:(id)arg1 bundle:(id)arg2
 {
-    NSLog(@"gdib_initWithNibName-IB %@, %@", arg1, arg2);
     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kStoryboardArea];
     return [self gdib_initWithNibName:arg1 bundle:arg2];
-}
-
--(void)gdib_noteUserDidExplicitlyChooseChoice:(id)arg1;
-{
-    NSLog(@"gdib_noteUserDidExplicitlyChooseChoice:%@",arg1);
-    [self gdib_noteUserDidExplicitlyChooseChoice:arg1];
-}
-
-- (void)gdib_userSelectedCategoryChoiceFromMenu:(id)arg1
-{
-    NSLog(@"gdib_userSelectedCategoryChoiceFromMenu %@",arg1);
-    [self gdib_userSelectedCategoryChoiceFromMenu:arg1];
 }
 
 
 #pragma mark - SourceCodePane
 - (id)gdib_initWithNibName:(id)arg1 bundle:(id)arg2 document:(id)arg3
 {
-    NSLog(@"gdib_initWithNibName %@, %@, %@", arg1, arg2, arg3);
     if([[NSUserDefaults standardUserDefaults] boolForKey:kStoryboardArea]){
         [[NSUserDefaults standardUserDefaults]setBool:NO forKey:kStoryboardArea];
     }
@@ -67,32 +48,14 @@
 - (id)gdib_selectedCategory
 {
     id selectedCategory = [self gdib_selectedCategory];
-    [self showUtilities];
+    if([[NSUserDefaults standardUserDefaults] boolForKey:kStoryboardArea]){
+        [[NSUserDefaults standardUserDefaults]setBool:NO forKey:kStoryboardArea];
+        [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:
+                                                                GDIBCanvasNotification object:self]];
+    }
     return selectedCategory;
 }
 
-
-- (void)gdib_toggleUtilitiesVisibility:(id)sender
-{
-    NSLog(@"gdib_toggleUtilitiesVisibility %@", sender);
-    [self gdib_toggleUtilitiesVisibility:sender];
-}
-
-- (void)gdib_showUtilitiesArea:(id)sender
-{
-    NSLog(@"gdib_showUtilitiesArea %@", sender);
-    [self gdib_showUtilitiesArea:sender];
-}
-
-- (void)showUtilities
-{
-    if ([[NSUserDefaults standardUserDefaults]boolForKey:kStoryboardArea] == NO) {
-        return;
-    }
-    [[NSUserDefaults standardUserDefaults]setBool:NO forKey:kStoryboardArea];
-    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:
-                                                            GDIBCanvasNotification object:self]];
-}
 
 @end
 
@@ -104,46 +67,23 @@
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
         if (NSClassFromString(@"IDESourceCodeEditor") != NULL) {
-//            NSLog(@"WORK ON HOOK NOW %@", plugin.description);
-            
             [NSClassFromString(@"IDESourceCodeEditor")
              jr_swizzleMethod:@selector(initWithNibName:bundle:document:)
              withMethod:@selector(gdib_initWithNibName:bundle:document:)
              error:NULL];
-            
+        }
+        if (NSClassFromString(@"IBCanvasViewController") != NULL){
             [NSClassFromString(@"IBCanvasViewController")
              jr_swizzleMethod:@selector(initWithNibName:bundle:)
              withMethod:@selector(gdib_initWithNibName:bundle:)
              error:NULL];
-		}
-        
-        if (NSClassFromString(@"IDEUtilityArea") != NULL) {
-//            NSLog(@"WORK ON HOOK NOW %@", plugin.description);
-            [NSClassFromString(@"IDEUtilityArea")
-             jr_swizzleMethod:@selector(noteUserDidExplicitlyChooseChoice:)
-             withMethod:@selector(gdib_noteUserDidExplicitlyChooseChoice:)
-             error:NULL];
             
+		}
+        if (NSClassFromString(@"IDEUtilityArea") != NULL) {
             [NSClassFromString(@"IDEUtilityArea")
              jr_swizzleMethod:@selector(selectedCategory)
              withMethod:@selector(gdib_selectedCategory)
              error:NULL];
-            
-            [NSClassFromString(@"IDEUtilityArea")
-             jr_swizzleMethod:@selector(userSelectedCategoryChoiceFromMenu:)
-             withMethod:@selector(gdib_userSelectedCategoryChoiceFromMenu:) error:NULL];
-        }
-        if (NSClassFromString(@"IDEWorkspaceTabController") != NULL) {
-            NSLog(@"IDEWorkspaceTabController");
-            [NSClassFromString(@"IDEWorkspaceTabController")
-             jr_swizzleMethod:@selector(toggleUtilitiesVisibility:)
-             withMethod:@selector(gdib_toggleUtilitiesVisibility:) error:NULL];
-            
-            
-            NSLog(@"IDEWorkspaceTabController");
-            [NSClassFromString(@"IDEWorkspaceTabController")
-             jr_swizzleMethod:@selector(showUtilitiesArea:)
-             withMethod:@selector(gdib_showUtilitiesArea:) error:NULL];
         }
 		sharedPlugin = [[self alloc] init];
 	});
@@ -162,16 +102,10 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification*)notification
 {
-    NSLog(@"XCODE DIDFINISH LAUNCHING");
+    NSLog(@"IB Sensor finish loading..");
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(showInspector:)
                                                  name:GDIBCanvasNotification object:nil];
-}
-
-- (void)activateFileInspector:(NSNotification*)notification
-{
-    NSDictionary *userInfo = [notification userInfo];
-    NSLog(@"GDBISensor userInfo %@", userInfo);
 }
 
 - (void)dealloc
@@ -182,26 +116,34 @@
 
 - (void)showInspector:(NSNotification*)notification
 {
-    NSBundle *pluginBundle = [NSBundle bundleForClass:[self class]];
-    
-    NSDictionary *errorInfo;
-    NSString *scriptlet = [pluginBundle pathForResource:@"OpenUtility" ofType:@"scpt"];
-    NSLog(@"scriptlet %@", scriptlet);
-    if ([scriptlet length] > 1) {
-        NSAppleScript *hack = [[[NSAppleScript alloc]initWithContentsOfURL: [NSURL fileURLWithPath:scriptlet]
-                                                                     error:&errorInfo]autorelease];
-        if (hack) {
-            NSLog(@"success in script");
-            NSAppleEventDescriptor *eventDescriptor = [hack executeAndReturnError:&errorInfo];
-            if (!eventDescriptor) {
-                NSLog(@"ERROR/SUCCESS %@", errorInfo);
-            }
-            else{
-                NSLog(@"ERROR %@", errorInfo);
+    [self performSelectorOnMainThread:@selector(executeScriptlet) withObject:nil waitUntilDone:NO];
+}
+
+- (void) executeScriptlet
+{
+    @try {
+        NSDictionary *errorInfo;
+        NSBundle *pluginBundle = [NSBundle bundleForClass:[self class]];
+        
+        NSString *scriptlet = [pluginBundle pathForResource:@"OpenUtility" ofType:@"scpt"];
+        NSLog(@"scriptlet %@", scriptlet);
+        if ([scriptlet length] > 1) {
+            NSAppleScript *hack = [[[NSAppleScript alloc]initWithContentsOfURL: [NSURL fileURLWithPath:scriptlet]
+                                                                         error:nil]autorelease];
+            if (hack) {
+                NSAppleEventDescriptor *eventDescriptor = [hack executeAndReturnError:&errorInfo];
+                if (!eventDescriptor) {
+                    NSLog(@"Error %@", errorInfo);
+                }
             }
         }
     }
-
+    @catch (NSException *exception) {
+        NSLog(@"%@", [exception debugDescription]);
+    }
+    @finally {
+        
+    }
 }
 
 @end
--- OpenUtility.applescript
+++ OpenUtility.applescript
@@ -5,39 +5,15 @@
 README 
 
 *)
--- Make sure
+(*tell application "Xcode"
+    activate
+end tell
+ *)
 tell application "Xcode"
-	tell active workspace document
-		set firstProject to (get first project)
-		set projectID to (get id of firstProject)
-		--set orgName to (get organization name of firstProject)
-		--set projDir to (get project directory of firstProject)
-		
-		-- activate tab bar to find file name
-		tell application "Xcode"
-			activate
-			set myFileName to (get name of window 1)
-			tell application "System Events"
-				tell application process "Xcode"
-					try
-					-- If its storyboad or xib open Inspector
-						if myFileName contains ".storyboard" OR myFileName contains ".xib" then
-							tell menu bar 1
-								tell menu bar item "View"
-									tell menu "View"
-										tell menu item "Utilities"
-											tell menu "Utilities"
-												click menu item "Show Identity Inspector"
-											end tell
-										end tell
-									end tell
-								end tell
-							end tell
-						end if
-						
-					end try
-				end tell
-			end tell
-		end tell
-	end tell
+tell active workspace document
+tell application "Xcode"
+activate
+tell application "System Events" to keystroke "0" using {command down, option down}
+end tell
+end tell
 end tell
